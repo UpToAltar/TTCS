396c14add6d574faeeb4d71a8238c2770936aab2
DaoMinhQuan27 <QuanDM.B22CN657@stu.ptit.edu.vn>
2025-03-19 23:33:16 +0700

    Tạo seed sẵn data 3 role

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import app from './app'
-import { sequelize } from './config/database';
+import { seedRoles, sequelize } from './config/database';
 import { config } from './config/env'
 import { logger } from './config/logger'
 
 const PORT = process.env.PORT || 5000;
 
 // Kết nối database
-sequelize.sync({ alter: true }).then(() => {
-  logger.info("Database connected & models synced!");
+sequelize.sync({ alter: true }).then(async () => {
+  logger.info('Database connected & models synced!')
+  await seedRoles() // Chạy seed
   app.listen(PORT, () => {
     logger.info(`Server is running on port http://localhost:${config.port}/api/swagger/`)
-  });
-});
+  })
+})
